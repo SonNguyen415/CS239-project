f96f91a107d251a8bed3f311a2de48905ea58217
Ramon <rtorre25@ucsc.edu>
2025-12-06 19:27:59 -0800

    added sql ycsb and cataloguedb

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ FILES=(
     "k8s-mongo/grafana.yaml"
     "k8s-mongo/user-db.yaml"
     "k8s-mongo/ycsb.yaml"
+    "sqlYCSB/catalogue-db-deployment.yaml"
+    "sqlYCSB/ycsb-interface-deployment.yaml"
 )
 
 # Function to apply a list of files
